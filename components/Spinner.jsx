c2c9c86ff2a0818340ae94ba9e3ece78e7f2dfe6
chellal aziz <azizchel.inf@gmail.com>
2024-01-18 17:54:30 +0100

    app completed

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-// import spinner from "../public/spinner.svg";
+import spinner from "../public/spinner.svg";
+import Image from "next/image";
 // import Image from "next/image";
 const Spinner = () => {
   {
     return (
       <>
-        {/* <Image
+        <Image
           src={spinner}
           className="w-[200px] m-auto block "
           alt="loading.."
-        /> */}
+        />
       </>
     );
   }
