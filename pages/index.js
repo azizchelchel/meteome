1aa18ccab8fba4c214faff043add4e24435055a9
chellal aziz <chellalaziz31@gmail.com>
2024-02-17 00:21:01 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Weather from "@/components/Weather";
 import Spinner from "@/components/Spinner";
+import bg_image from "@/public/assets/images/bg.jpg";
 
 export default function Home() {
   const [city, setCity] = useState("");
@@ -30,21 +31,47 @@ export default function Home() {
       return <Spinner />;
     } else {
       return (
-        <div className="">
+        <div className="relative h-screen">
           {/* overlay */}
-          <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/40 z" />
+          <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/50 z-0" />
           {/* background image */}
           <Image
-            src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?q=80&w=1550&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={bg_image}
             layout="fill"
-            className="object-cover "
+            className=" object-cover "
             alt="background image"
           />
+
           {/* search */}
-          <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white ">
+          <div className="relative  flex  justify-center items-center max-w-[500px] sm:w-screen sm:max-w-prose m-auto  gap-5 py-3  text-white ">
+            <button
+              // onClick={() => {
+              //   signOut();
+              // }}
+              href="#"
+              className="sm:hidden   sm:absolute sm:justify-center sm:left-0 sm:items-center sm:py-3 sm:px-1 sm:text-base sm:text-center sm:text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            >
+              Logout
+              <svg
+                class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </button>
+
             <form
               onSubmit={fetchWeather}
-              className="flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl "
+              className=" flex justify-between sm:max-w-[500px] items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl "
             >
               <div>
                 <input
@@ -60,7 +87,34 @@ export default function Home() {
                 <BsSearch size={20} />
               </button>
             </form>
+
+            <button
+              // onClick={() => {
+              //   signOut();
+              // }}
+              href="#"
+              // className=" sm:absolute sm:right-1 inline-flex justify-center items-center py-3 px-2 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              className="sm:hidden"
+            >
+              Logout
+              <svg
+                class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </button>
           </div>
+
           {/* weather */}
           {weather.main && <Weather data={weather} />}
         </div>
